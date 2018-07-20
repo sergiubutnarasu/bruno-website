0dde9e506994e37e837a11cfc2ce804df6666fe4
Sergiu Butnarasu <sergiu.butnarasu@gmail.com>
2018-07-20 21:32:01 +0300

    Button pack with highlight js

--- src/components.d.ts
+++ src/components.d.ts
@@ -25,8 +25,9 @@ declare global {
   interface HTMLAttributes {}
 }
 
-import '@stencil/router';
 import 'bruno-ui';
+import '@stencil/router';
+import '@stencil/state-tunnel';
 
 
 declare global {
--- src/components/app-buttons/app-buttons.tsx
+++ src/components/app-buttons/app-buttons.tsx
@@ -1,28 +1,83 @@
-import { Component } from "../../../node_modules/@stencil/core";
+import {
+  Component,
+  Element,
+  ComponentDidLoad
+} from "../../../node_modules/@stencil/core";
+import hljs from "highlight.js";
 
 @Component({
   tag: "app-buttons"
 })
-export class AppButtons {
+export class AppButtons implements ComponentDidLoad {
+  @Element() _element: HTMLElement;
+
+  componentDidLoad() {
+    hljs.initHighlighting();
+  }
+
   render() {
     return (
       <div>
-        <h3>Buttons</h3>
+        <div class="display display--large margin-bottom">Buttons</div>
         <p class="margin-bottom">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-          atque?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem, iure rerum
+          reprehenderit, doloribus quaerat repudiandae expedita pariatur quidem
+          aliquam consectetur, nisi accusantium deserunt possimus! Dignissimos
+          porro quis et fuga quam! atque?
+        </p>
+
+        <hr />
+
+        <h5 class="margin-bottom">Default</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
         </p>
 
         <div class="margin-bottom child-margin-right">
-          <div class="brn-button brn-button--primary ">Primary</div>
-          <div class="brn-button brn-button--secondary ">Secondary</div>
-          <div class="brn-button brn-button--success ">Success</div>
-          <div class="brn-button brn-button--danger ">Danger</div>
-          <div class="brn-button brn-button--warning ">Warning</div>
-          <div class="brn-button brn-button--info ">Info</div>
-          <div class="brn-button brn-button--light ">Light</div>
+          <div class="brn-button brn-button--primary">Primary</div>
+          <div class="brn-button brn-button--secondary">Secondary</div>
+          <div class="brn-button brn-button--success">Success</div>
+          <div class="brn-button brn-button--danger">Danger</div>
+          <div class="brn-button brn-button--warning">Warning</div>
+          <div class="brn-button brn-button--info">Info</div>
+          <div class="brn-button brn-button--light">Light</div>
           <div class="brn-button brn-button--dark">Dark</div>
         </div>
+
+        <pre>
+          <code class="html">
+            {'<div class="brn-button brn-button--primary">Primary</div>'}
+            <br />
+            {'<div class="brn-button brn-button--secondary">Secondary</div>'}
+            <br />
+            {'<div class="brn-button brn-button--success">Success</div>'}
+            <br />
+            {'<div class="brn-button brn-button--danger">Danger</div>'}
+            <br />
+            {'<div class="brn-button brn-button--warning">Warning</div>'}
+            <br />
+            {'<div class="brn-button brn-button--info">Info</div>'}
+            <br />
+            {'<div class="brn-button brn-button--light">Light</div>'}
+            <br />
+            {'<div class="brn-button brn-button--dark">Dark</div>'}
+          </code>
+        </pre>
+
+        <hr />
+
+        <h5 class="margin-bottom">Outline</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
+        </p>
 
         <div class="margin-bottom child-margin-right">
           <div class="brn-button brn-button--primary brn-button--outline">
@@ -51,6 +106,52 @@ export class AppButtons {
           </div>
         </div>
 
+        <pre>
+          <code class="html">
+            {
+              '<div class="brn-button brn-button--primary brn-button--outline">Primary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--secondary brn-button--outline">Secondary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--success brn-button--outline">Success</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--danger brn-button--outline">Danger</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--warning brn-button--outline">Warning</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--info brn-button--outline">Info</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--light brn-button--outline">Light</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--dark brn-button--outline">Dark</div>'
+            }
+          </code>
+        </pre>
+
+        <hr />
+
+        <h5 class="margin-bottom">Clean</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
+        </p>
+
         <div class="margin-bottom child-margin-right">
           <div class="brn-button brn-button--primary brn-button--clean">
             Primary
@@ -73,6 +174,52 @@ export class AppButtons {
           </div>
           <div class="brn-button brn-button--dark brn-button--clean">Dark</div>
         </div>
+
+        <pre>
+          <code class="html">
+            {
+              '<div class="brn-button brn-button--primary brn-button--clean">Primary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--secondary brn-button--clean">Secondary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--success brn-button--clean">Success</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--danger brn-button--clean">Danger</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--warning brn-button--clean">Warning</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--info brn-button--clean">Info</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--light brn-button--clean">Light</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--dark brn-button--clean">Dark</div>'
+            }
+          </code>
+        </pre>
+
+        <hr />
+
+        <h5 class="margin-bottom">Rounded</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
+        </p>
 
         <div class="margin-bottom child-margin-right">
           <div class="brn-button brn-button--primary brn-button--rounded">
@@ -101,6 +248,52 @@ export class AppButtons {
           </div>
         </div>
 
+        <pre>
+          <code class="html">
+            {
+              '<div class="brn-button brn-button--primary brn-button--rounded">Primary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--secondary brn-button--rounded">Secondary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--success brn-button--rounded">Success</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--danger brn-button--rounded">Danger</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--warning brn-button--rounded">Warning</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--info brn-button--rounded">Info</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--light brn-button--rounded">Light</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--dark brn-button--rounded">Dark</div>'
+            }
+          </code>
+        </pre>
+
+        <hr />
+
+        <h5 class="margin-bottom">Rounded and outline</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
+        </p>
+
         <div class="margin-bottom child-margin-right">
           <div class="brn-button brn-button--primary brn-button--rounded brn-button--outline">
             Primary
@@ -128,6 +321,52 @@ export class AppButtons {
           </div>
         </div>
 
+        <pre>
+          <code class="html">
+            {
+              '<div class="brn-button brn-button--primary brn-button--rounded brn-button--outline">Primary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--secondary brn-button--rounded brn-button--outline">Secondary</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--success brn-button--rounded brn-button--outline">Success</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--danger brn-button--rounded brn-button--outline">Danger</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--warning brn-button--rounded brn-button--outline">Warning</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--info brn-button--rounded brn-button--outline">Info</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--light brn-button--rounded brn-button--outline">Light</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--dark brn-button--rounded brn-button--outline">Dark</div>'
+            }
+          </code>
+        </pre>
+
+        <hr />
+
+        <h5 class="margin-bottom">Block</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
+        </p>
+
         <div class="margin-bottom child-margin-right">
           <brn-row>
             <brn-column columns={1} total={3}>
@@ -148,6 +387,25 @@ export class AppButtons {
           </brn-row>
         </div>
 
+        <pre>
+          <code class="html">
+            {
+              '<div class="brn-button brn-button--primary brn-button--block">Primary</div>'
+            }
+            <br />
+          </code>
+        </pre>
+
+        <hr />
+
+        <h5 class="margin-bottom">Sizes</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
+        </p>
+
         <div class="margin-bottom child-margin-right">
           <div class="brn-button brn-button--secondary brn-button--small">
             Small
@@ -160,6 +418,33 @@ export class AppButtons {
           </div>
         </div>
 
+        <pre>
+          <code class="html">
+            {
+              '<div class="brn-button brn-button--secondary brn-button--small">Small</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--secondary brn-button--medium">Medium</div>'
+            }
+            <br />
+            {
+              '<div class="brn-button brn-button--secondary brn-button--large">Large</div>'
+            }
+            <br />
+          </code>
+        </pre>
+
+        <hr />
+
+        <h5 class="margin-bottom">With Icons</h5>
+
+        <p class="margin-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam
+          iste facere dolor deleniti similique sequi eligendi. Numquam at enim
+          alias amet esse harum illo fugiat aspernatur ea autem.
+        </p>
+
         <div class="margin-bottom child-margin-right">
           <div class="brn-button brn-button--primary">
             <i class="brn-button__icon ion-ios-git-branch" /> Lorem ipsum
@@ -170,6 +455,20 @@ export class AppButtons {
             <i class="brn-button__icon ion-ios-contact" />
           </div>
         </div>
+
+        <pre>
+          <code class="html">
+            {`<div class="brn-button brn-button--primary">
+  <i class="brn-button__icon ion-ios-git-branch" /> Lorem ipsum
+</div>`}
+            <br />
+            <br />
+            {`<div class="brn-button brn-button--light brn-button__icon--right">
+  Lorem ipsum <i class="brn-button__icon ion-ios-contact" />
+</div>`}
+            <br />
+          </code>
+        </pre>
       </div>
     );
   }
